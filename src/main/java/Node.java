35654dc7e7a69d22d3005885868309c375456fcf
KeerthanaBanukumar <>
2025-01-24 15:57:24 +0800

    Level - 0 Increment

---
+++ src/main/java/Node.java
@@ -0,0 +1,17 @@
+public class Node {
+    public static void main(String[] args) {
+        String logo = " _   _           _     \n"
+                + "| \\ | | ___   __| | ___ \n"
+                + "|  \\| |/ _ \\ / _` |/ _ \\\n"
+                + "| |\\  | (_) | (_| |  __/\n"
+                + "|_| \\_|\\___/ \\__,_|\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Node");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+    }
+}
